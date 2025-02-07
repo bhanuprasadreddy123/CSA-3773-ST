f2738531ae641aaa869489ce37b2fbe4f583e175
m.Bhanu prasad reddy <124137568+bhanuprasadreddy123@users.noreply.github.com>
2025-02-07 13:45:42 +0530

    Add files via upload

---
+++ Experiment 8.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 5; 
+        long factorial = 1; 
+        
+        for (int i = 1; i <= number; ++i) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + number + " = " + factorial);
+    }
+}
